18656ad1a864c453b12b17bfb1e6bb8ed3400478
unknown <brandenek03912@hotmail.com>
2024-05-04 08:48:09 -0400

    ui: add left and right panel

--- Blasphemous.Modding.SkinEditor/MainForm.Designer.cs
+++ Blasphemous.Modding.SkinEditor/MainForm.Designer.cs
@@ -30,16 +30,41 @@ partial class MainForm
     {
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
         UI = new Panel();
+        _right = new Panel();
+        _left = new Panel();
+        UI.SuspendLayout();
         SuspendLayout();
         // 
         // UI
         // 
         UI.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
         UI.BackColor = Color.Fuchsia;
+        UI.Controls.Add(_right);
+        UI.Controls.Add(_left);
         UI.Location = new Point(30, 15);
         UI.Name = "UI";
         UI.Size = new Size(1200, 650);
         UI.TabIndex = 0;
+        // 
+        // _right
+        // 
+        _right.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        _right.BackColor = Color.FromArgb(64, 64, 64);
+        _right.BorderStyle = BorderStyle.Fixed3D;
+        _right.Location = new Point(300, 0);
+        _right.Name = "_right";
+        _right.Size = new Size(900, 650);
+        _right.TabIndex = 1;
+        // 
+        // _left
+        // 
+        _left.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+        _left.BackColor = Color.Silver;
+        _left.BorderStyle = BorderStyle.Fixed3D;
+        _left.Location = new Point(0, 0);
+        _left.Name = "_left";
+        _left.Size = new Size(300, 650);
+        _left.TabIndex = 0;
         // 
         // MainForm
         // 
@@ -54,10 +79,13 @@ partial class MainForm
         FormClosing += OnFormClose;
         Load += OnFormOpen;
         SizeChanged += OnFormResized;
+        UI.ResumeLayout(false);
         ResumeLayout(false);
     }
 
     #endregion
 
     private Panel UI;
+    private Panel _left;
+    private Panel _right;
 }
