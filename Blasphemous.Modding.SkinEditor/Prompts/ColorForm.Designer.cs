0cfbc36acb045a831ab61b0ea5adc6d31d8d45bf
unknown <brandenek03912@hotmail.com>
2024-05-06 16:46:49 -0400

    ui: add rgb sliders to color form

--- Blasphemous.Modding.SkinEditor/Prompts/ColorForm.Designer.cs
+++ Blasphemous.Modding.SkinEditor/Prompts/ColorForm.Designer.cs
@@ -30,14 +30,37 @@ partial class ColorForm
     {
         _buttons_confirm = new Button();
         _buttons_cancel = new Button();
+        _preview_color = new Panel();
+        _r_slider = new TrackBar();
+        _r_text = new TextBox();
+        _r_label = new Label();
+        _preview = new Panel();
+        _r = new Panel();
+        _g = new Panel();
+        _g_slider = new TrackBar();
+        _g_text = new TextBox();
+        _g_label = new Label();
+        _buttons = new Panel();
+        _b = new Panel();
+        _b_slider = new TrackBar();
+        _b_text = new TextBox();
+        _b_label = new Label();
+        ((System.ComponentModel.ISupportInitialize)_r_slider).BeginInit();
+        _preview.SuspendLayout();
+        _r.SuspendLayout();
+        _g.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)_g_slider).BeginInit();
+        _buttons.SuspendLayout();
+        _b.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)_b_slider).BeginInit();
         SuspendLayout();
         // 
         // _buttons_confirm
         // 
-        _buttons_confirm.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        _buttons_confirm.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         _buttons_confirm.AutoSize = true;
         _buttons_confirm.DialogResult = DialogResult.OK;
-        _buttons_confirm.Location = new Point(216, 224);
+        _buttons_confirm.Location = new Point(166, 6);
         _buttons_confirm.Name = "_buttons_confirm";
         _buttons_confirm.Size = new Size(75, 25);
         _buttons_confirm.TabIndex = 0;
@@ -46,15 +69,162 @@ partial class ColorForm
         // 
         // _buttons_cancel
         // 
-        _buttons_cancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        _buttons_cancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         _buttons_cancel.AutoSize = true;
         _buttons_cancel.DialogResult = DialogResult.Cancel;
-        _buttons_cancel.Location = new Point(297, 224);
+        _buttons_cancel.Location = new Point(247, 6);
         _buttons_cancel.Name = "_buttons_cancel";
         _buttons_cancel.Size = new Size(75, 25);
         _buttons_cancel.TabIndex = 1;
         _buttons_cancel.Text = "Cancel";
         _buttons_cancel.UseVisualStyleBackColor = true;
+        // 
+        // _preview_color
+        // 
+        _preview_color.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+        _preview_color.BackColor = Color.Black;
+        _preview_color.Location = new Point(10, 10);
+        _preview_color.Name = "_preview_color";
+        _preview_color.Size = new Size(100, 100);
+        _preview_color.TabIndex = 2;
+        // 
+        // _r_slider
+        // 
+        _r_slider.Location = new Point(80, 0);
+        _r_slider.Maximum = 255;
+        _r_slider.Name = "_r_slider";
+        _r_slider.Size = new Size(250, 45);
+        _r_slider.TabIndex = 3;
+        // 
+        // _r_text
+        // 
+        _r_text.Location = new Point(30, 0);
+        _r_text.MaxLength = 2;
+        _r_text.Name = "_r_text";
+        _r_text.Size = new Size(40, 23);
+        _r_text.TabIndex = 4;
+        // 
+        // _r_label
+        // 
+        _r_label.AutoSize = true;
+        _r_label.Location = new Point(10, 0);
+        _r_label.Name = "_r_label";
+        _r_label.Size = new Size(17, 15);
+        _r_label.TabIndex = 5;
+        _r_label.Text = "R:";
+        _r_label.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // _preview
+        // 
+        _preview.BackColor = SystemColors.ControlLight;
+        _preview.Controls.Add(_preview_color);
+        _preview.Dock = DockStyle.Top;
+        _preview.Location = new Point(0, 0);
+        _preview.Name = "_preview";
+        _preview.Size = new Size(334, 120);
+        _preview.TabIndex = 9;
+        // 
+        // _r
+        // 
+        _r.AutoSize = true;
+        _r.BackColor = SystemColors.ControlLight;
+        _r.Controls.Add(_r_slider);
+        _r.Controls.Add(_r_text);
+        _r.Controls.Add(_r_label);
+        _r.Dock = DockStyle.Top;
+        _r.Location = new Point(0, 120);
+        _r.Name = "_r";
+        _r.Size = new Size(334, 48);
+        _r.TabIndex = 10;
+        // 
+        // _g
+        // 
+        _g.AutoSize = true;
+        _g.BackColor = SystemColors.ControlLight;
+        _g.Controls.Add(_g_slider);
+        _g.Controls.Add(_g_text);
+        _g.Controls.Add(_g_label);
+        _g.Dock = DockStyle.Top;
+        _g.Location = new Point(0, 168);
+        _g.Name = "_g";
+        _g.Size = new Size(334, 48);
+        _g.TabIndex = 11;
+        // 
+        // _g_slider
+        // 
+        _g_slider.Location = new Point(80, 0);
+        _g_slider.Maximum = 255;
+        _g_slider.Name = "_g_slider";
+        _g_slider.Size = new Size(250, 45);
+        _g_slider.TabIndex = 3;
+        // 
+        // _g_text
+        // 
+        _g_text.Location = new Point(30, 0);
+        _g_text.MaxLength = 2;
+        _g_text.Name = "_g_text";
+        _g_text.Size = new Size(40, 23);
+        _g_text.TabIndex = 4;
+        // 
+        // _g_label
+        // 
+        _g_label.AutoSize = true;
+        _g_label.Location = new Point(10, 0);
+        _g_label.Name = "_g_label";
+        _g_label.Size = new Size(18, 15);
+        _g_label.TabIndex = 5;
+        _g_label.Text = "G:";
+        _g_label.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // _buttons
+        // 
+        _buttons.AutoSize = true;
+        _buttons.Controls.Add(_buttons_cancel);
+        _buttons.Controls.Add(_buttons_confirm);
+        _buttons.Dock = DockStyle.Top;
+        _buttons.Location = new Point(0, 264);
+        _buttons.Name = "_buttons";
+        _buttons.Size = new Size(334, 34);
+        _buttons.TabIndex = 12;
+        // 
+        // _b
+        // 
+        _b.AutoSize = true;
+        _b.BackColor = SystemColors.ControlLight;
+        _b.Controls.Add(_b_slider);
+        _b.Controls.Add(_b_text);
+        _b.Controls.Add(_b_label);
+        _b.Dock = DockStyle.Top;
+        _b.Location = new Point(0, 216);
+        _b.Name = "_b";
+        _b.Size = new Size(334, 48);
+        _b.TabIndex = 11;
+        // 
+        // _b_slider
+        // 
+        _b_slider.Location = new Point(80, 0);
+        _b_slider.Maximum = 255;
+        _b_slider.Name = "_b_slider";
+        _b_slider.Size = new Size(250, 45);
+        _b_slider.TabIndex = 3;
+        // 
+        // _b_text
+        // 
+        _b_text.Location = new Point(30, 0);
+        _b_text.MaxLength = 2;
+        _b_text.Name = "_b_text";
+        _b_text.Size = new Size(40, 23);
+        _b_text.TabIndex = 4;
+        // 
+        // _b_label
+        // 
+        _b_label.AutoSize = true;
+        _b_label.Location = new Point(10, 0);
+        _b_label.Name = "_b_label";
+        _b_label.Size = new Size(17, 15);
+        _b_label.TabIndex = 5;
+        _b_label.Text = "B:";
+        _b_label.TextAlign = ContentAlignment.MiddleRight;
         // 
         // ColorForm
         // 
@@ -62,9 +232,12 @@ partial class ColorForm
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         CancelButton = _buttons_cancel;
-        ClientSize = new Size(384, 261);
-        Controls.Add(_buttons_cancel);
-        Controls.Add(_buttons_confirm);
+        ClientSize = new Size(334, 301);
+        Controls.Add(_buttons);
+        Controls.Add(_b);
+        Controls.Add(_g);
+        Controls.Add(_r);
+        Controls.Add(_preview);
         FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
@@ -74,6 +247,18 @@ partial class ColorForm
         StartPosition = FormStartPosition.CenterParent;
         Text = "Choose a color";
         TopMost = true;
+        ((System.ComponentModel.ISupportInitialize)_r_slider).EndInit();
+        _preview.ResumeLayout(false);
+        _r.ResumeLayout(false);
+        _r.PerformLayout();
+        _g.ResumeLayout(false);
+        _g.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)_g_slider).EndInit();
+        _buttons.ResumeLayout(false);
+        _buttons.PerformLayout();
+        _b.ResumeLayout(false);
+        _b.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)_b_slider).EndInit();
         ResumeLayout(false);
         PerformLayout();
     }
@@ -82,4 +267,19 @@ partial class ColorForm
 
     private Button _buttons_confirm;
     private Button _buttons_cancel;
+    private Panel _preview_color;
+    private TrackBar _r_slider;
+    private TextBox _r_text;
+    private Label _r_label;
+    private Panel _preview;
+    private Panel _r;
+    private Panel _g;
+    private TrackBar _g_slider;
+    private TextBox _g_text;
+    private Label _g_label;
+    private Panel _buttons;
+    private Panel _b;
+    private TrackBar _b_slider;
+    private TextBox _b_text;
+    private Label _b_label;
 }
