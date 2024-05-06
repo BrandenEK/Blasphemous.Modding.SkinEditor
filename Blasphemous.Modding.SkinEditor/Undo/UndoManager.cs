77075cfffa9c94a184ee4a55f2aa588d9f2543ae
unknown <brandenek03912@hotmail.com>
2024-05-06 08:49:22 -0400

    undo: add manager

---
+++ Blasphemous.Modding.SkinEditor/Undo/UndoManager.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Blasphemous.Modding.SkinEditor.Undo;
+
+public class UndoManager
+{
+    private readonly List<IUndoCommand> undo = new();
+    private readonly List<IUndoCommand> redo = new();
+
+    public void Reset()
+    {
+        undo.Clear();
+        redo.Clear();
+    }
+
+    public void Do(IUndoCommand command)
+    {
+        undo.Add(command);
+        if (undo.Count > MAX_STACK_SIZE)
+            undo.RemoveAt(0);
+        redo.Clear();
+    }
+
+    public IUndoCommand Undo()
+    {
+        if (undo.Count == 0)
+            return null;
+
+        IUndoCommand command = undo[undo.Count - 1];
+        undo.RemoveAt(undo.Count - 1);
+        redo.Add(command);
+        return command;
+    }
+
+    public IUndoCommand Redo()
+    {
+        if (redo.Count == 0)
+            return null;
+
+        IUndoCommand command = redo[redo.Count - 1];
+        redo.RemoveAt(redo.Count - 1);
+        undo.Add(command);
+        return command;
+    }
+
+    private const int MAX_STACK_SIZE = 20;
+}
