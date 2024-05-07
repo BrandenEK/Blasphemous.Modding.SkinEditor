4afc7e0e8ae5598f94193b2373549c194544365e
unknown <brandenek03912@hotmail.com>
2024-05-06 22:00:54 -0400

    ui: add first text section

--- Blasphemous.Modding.SkinEditor/Prompts/InfoPrompt.Designer.cs
+++ Blasphemous.Modding.SkinEditor/Prompts/InfoPrompt.Designer.cs
@@ -31,7 +31,11 @@ partial class InfoPrompt
         _buttons = new Panel();
         _buttons_cancel = new Button();
         _buttons_confirm = new Button();
+        _id = new Panel();
+        _id_text = new TextBox();
+        _id_label = new Label();
         _buttons.SuspendLayout();
+        _id.SuspendLayout();
         SuspendLayout();
         // 
         // _buttons
@@ -40,9 +44,9 @@ partial class InfoPrompt
         _buttons.Controls.Add(_buttons_cancel);
         _buttons.Controls.Add(_buttons_confirm);
         _buttons.Dock = DockStyle.Top;
-        _buttons.Location = new Point(0, 0);
+        _buttons.Location = new Point(0, 58);
         _buttons.Name = "_buttons";
-        _buttons.Size = new Size(232, 34);
+        _buttons.Size = new Size(237, 34);
         _buttons.TabIndex = 9;
         // 
         // _buttons_cancel
@@ -50,7 +54,7 @@ partial class InfoPrompt
         _buttons_cancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         _buttons_cancel.AutoSize = true;
         _buttons_cancel.DialogResult = DialogResult.Cancel;
-        _buttons_cancel.Location = new Point(145, 6);
+        _buttons_cancel.Location = new Point(150, 6);
         _buttons_cancel.Name = "_buttons_cancel";
         _buttons_cancel.Size = new Size(75, 25);
         _buttons_cancel.TabIndex = 5;
@@ -62,12 +66,48 @@ partial class InfoPrompt
         _buttons_confirm.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         _buttons_confirm.AutoSize = true;
         _buttons_confirm.DialogResult = DialogResult.OK;
-        _buttons_confirm.Location = new Point(64, 6);
+        _buttons_confirm.Location = new Point(69, 6);
         _buttons_confirm.Name = "_buttons_confirm";
         _buttons_confirm.Size = new Size(75, 25);
         _buttons_confirm.TabIndex = 4;
         _buttons_confirm.Text = "Confirm";
         _buttons_confirm.UseVisualStyleBackColor = true;
+        // 
+        // _id
+        // 
+        _id.AutoSize = true;
+        _id.BackColor = SystemColors.ControlLight;
+        _id.Controls.Add(_id_text);
+        _id.Controls.Add(_id_label);
+        _id.Dock = DockStyle.Top;
+        _id.Location = new Point(0, 0);
+        _id.Name = "_id";
+        _id.Padding = new Padding(10, 0, 10, 10);
+        _id.Size = new Size(237, 58);
+        _id.TabIndex = 10;
+        // 
+        // _id_text
+        // 
+        _id_text.CharacterCasing = CharacterCasing.Upper;
+        _id_text.Dock = DockStyle.Top;
+        _id_text.Location = new Point(10, 25);
+        _id_text.MaxLength = 32;
+        _id_text.Name = "_id_text";
+        _id_text.PlaceholderText = "PENITENT_DM_EXAMPLE";
+        _id_text.Size = new Size(217, 23);
+        _id_text.TabIndex = 1;
+        // 
+        // _id_label
+        // 
+        _id_label.Dock = DockStyle.Top;
+        _id_label.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+        _id_label.Location = new Point(10, 0);
+        _id_label.Name = "_id_label";
+        _id_label.Padding = new Padding(0, 3, 0, 0);
+        _id_label.Size = new Size(217, 25);
+        _id_label.TabIndex = 0;
+        _id_label.Text = "Skin Id:";
+        _id_label.TextAlign = ContentAlignment.TopCenter;
         // 
         // InfoPrompt
         // 
@@ -75,8 +115,9 @@ partial class InfoPrompt
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         CancelButton = _buttons_cancel;
-        ClientSize = new Size(232, 244);
+        ClientSize = new Size(237, 244);
         Controls.Add(_buttons);
+        Controls.Add(_id);
         FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
@@ -87,6 +128,8 @@ partial class InfoPrompt
         TopMost = true;
         _buttons.ResumeLayout(false);
         _buttons.PerformLayout();
+        _id.ResumeLayout(false);
+        _id.PerformLayout();
         ResumeLayout(false);
         PerformLayout();
     }
@@ -96,4 +139,7 @@ partial class InfoPrompt
     private Panel _buttons;
     private Button _buttons_cancel;
     private Button _buttons_confirm;
+    private Panel _id;
+    private Label _id_label;
+    private TextBox _id_text;
 }
