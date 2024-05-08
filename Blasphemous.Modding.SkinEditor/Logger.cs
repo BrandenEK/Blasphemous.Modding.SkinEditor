74c37d37285c2dcebc6cd784ac925d6766042d83
unknown <brandenek03912@hotmail.com>
2024-05-07 21:49:03 -0400

    log: also log to file in data folder

--- Blasphemous.Modding.SkinEditor/Logger.cs
+++ Blasphemous.Modding.SkinEditor/Logger.cs
@@ -4,22 +4,65 @@ namespace Blasphemous.Modding.SkinEditor;
 
 public static class Logger
 {
+    private static readonly string _logPath = Path.Combine(Core.DataFolder, "Latest.log");
+
     public static void Initialize()
     {
-        AttachConsole(-1);
-        Info(string.Empty);
+        InitConsole();
+        InitFile();
     }
 
-    public static void Info(object message) => Log(message, ConsoleColor.White);
+    public static void Info(object message)
+    {
+        LogToConsole(message, ConsoleColor.White);
+        LogtoFile(message, "Info");
+    }
 
-    public static void Warn(object message) => Log(message, ConsoleColor.Yellow);
+    public static void Warn(object message)
+    {
+        LogToConsole(message, ConsoleColor.Yellow);
+        LogtoFile(message, "Warning");
+    }
 
-    public static void Error(object message) => Log(message, ConsoleColor.Red);
+    public static void Error(object message)
+    {
+        LogToConsole(message, ConsoleColor.Red);
+        LogtoFile(message, "Error");
+    }
 
-    private static void Log(object message, ConsoleColor color)
+    // Console
+
+    private static void InitConsole()
+    {
+        AttachConsole(-1);
+    }
+
+    private static void LogToConsole(object message, ConsoleColor color)
     {
         Console.ForegroundColor = color;
         Console.WriteLine(message);
+    }
+
+    // File
+
+    private static void InitFile()
+    {
+        if (File.Exists(_logPath))
+            File.SetAttributes(_logPath, File.GetAttributes(_logPath) & ~FileAttributes.ReadOnly);
+
+        File.WriteAllText(_logPath, string.Empty);
+        File.SetAttributes(_logPath, FileAttributes.ReadOnly);
+    }
+
+    private static void LogtoFile(object message, string type)
+    {
+        string output = $"[{DateTime.Now:HH:mm:ss} {type}] {message}{Environment.NewLine}";
+
+        if (File.Exists(_logPath))
+            File.SetAttributes(_logPath, File.GetAttributes(_logPath) & ~FileAttributes.ReadOnly);
+
+        File.AppendAllText(_logPath, output);
+        File.SetAttributes(_logPath, FileAttributes.ReadOnly);
     }
 
     [DllImport("kernel32", SetLastError = true)]
